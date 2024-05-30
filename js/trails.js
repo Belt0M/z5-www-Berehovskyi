172bd45f097271c2554171c769c6d22b52b4671e
Belt0M <belongtomars@gmail.com>
2024-05-30 17:23:34 +0200

    Finish trails section (Pop-up added)

--- js/trails.js
+++ js/trails.js
@@ -1,29 +1,61 @@
-const TRAILS_DATA = [
-	{name: 'Szlak Bursztynowy', desc: 'Some desc'},
-	{name: 'Szlak Grunwaldzki', desc: 'Some desc'},
-	{name: 'Szlak Gorących Żródeł', desc: 'Some desc'},
-	{name: 'Szlak Rowerowy w Centrum Polski', desc: 'Some desc'},
-	{name: 'Łódzka Magistrala W-E', desc: 'Some desc'},
-	{name: 'Łódzka Magistrala N-S', desc: 'Some desc'},
-]
+let TRAILS_DATA, popUpWindow
 
 // Trails mapping
 
-window.onload = function () {
+window.onload = async function () {
+	const response = await fetch('../data/trails.json')
+
+	const json = await response.json()
+	TRAILS_DATA = JSON.parse(json)
+
+	popUpWindow = document.querySelector('.pop-up')
+
+	if (popUpWindow) {
+		popUpWindow.addEventListener('click', e => {
+			if (e.target && e.target.className && e.target.className === 'pop-up') {
+				popUpWindow.style.display = 'none'
+			}
+		})
+	}
+
 	if (TRAILS_DATA) {
 		const trailsContainer = document.querySelector('.trails_container')
 
 		if (trailsContainer) {
-			TRAILS_DATA.forEach((trailsObj, index) => {
+			TRAILS_DATA.forEach((trailObj, index) => {
 				const div = document.createElement('div')
 
 				div.className = `trail trail_${index + 1}`
 				div.style.backgroundImage = `url('../img/trail_${index + 1}.jpg')`
 
 				div.innerHTML = `
-				<h3>${trailsObj.name}</h3>
-				<span class='desc'>${trailsObj.desc}</span>
+				<h3>${trailObj.name}</h3>
+				<span class='desc'>${trailObj.desc}</span>
 				`
+
+				div.onclick = function () {
+					if (!popUpWindow) {
+						popUpWindow = document.querySelector('.pop-up')
+					}
+
+					if (popUpWindow) {
+						popUpWindow.style.display = 'block'
+
+						const content = popUpWindow.querySelector('.content')
+
+						if (content) {
+							content.innerHTML = `
+							<img src='../img/trail_${index + 1}.jpg'/>
+							<h2>${trailObj.name}</h2>
+							<span>Województwo łódzkie</span>
+							<p>${trailObj.about}</p>
+							<iframe src="${
+								trailObj.mapLink
+							}" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+							`
+						}
+					}
+				}
 
 				const rand = Math.floor(Math.random() * 45) + 5
 				const rand2 = Math.floor(Math.random() * 359)
